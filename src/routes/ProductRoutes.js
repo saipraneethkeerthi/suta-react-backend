ce5d9568d70aaf20a48490360917509f18ae0a62
Keerthi Sai Praneeth <kpraneeth@innominds.com>
2021-11-23 00:16:54 +0530

    error condition check methods

--- src/routes/ProductRoutes.js
+++ src/routes/ProductRoutes.js
@@ -1,12 +1,19 @@
 //Importing express and router
 const express = require("express");
-const product = require("../schema/productschema");
+const product = require("../Schema/ProductSchema");
 const config = require("../config.json");
 const jwt = require("jsonwebtoken");
 const router = new express.Router();
 
 router.get("/products", (req, res) => {
-	product.find({}).then((data) => res.status(200).send(data));
+	product.find({}).then((data) =>{
+		if(data){
+			res.status(200).send(data)
+		}
+		else{
+			res.status(404).send("No Data Found")
+		}
+	});
 });
 
 router.get("/products/id", (req, res) => {
--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -3,15 +3,19 @@ const express = require("express");
 const user = require("../Schema/UserSchema");
 const config = require("../config.json");
 const jwt = require("jsonwebtoken");
+const {validateEmail, validatePassword} = require("../middlewares/middlewares")
+
+
+
 const router = new express.Router();
 
-router.get("/userGetData", (req, res) => {
+router.get("/users", (req, res) => {
 	console.log("database post");
 	const storeddata = user.find({});
 	console.log(storeddata);
 	res.status(200).send("");
 });
-router.post("/signup", (req, res) => {
+router.post("/signup",validateEmail,validatePassword, (req, res) => {
 	const body = req.body;
 	console.log(body);
 	user.insertMany(req.body).then((data) => res.status(200).send(data));
@@ -20,15 +24,20 @@ router.post("/login", (req, res) => {
   const body = req.body;
   // console.log(body);
   user.findOne(body).then((data) => {
-    // console.log(data)
+    console.log(data)
+    if(data){
     const tokenGen = jwt.sign({ sub: data._doc._id }, config.secret, {
       expiresIn: "7d",
     });
     const newData = { ...data._doc, token: tokenGen };
     // data.token=tokenGen
-    // console.log(newData) 
-	
+    console.log(newData) 
 		res.status(200).send(newData);
+
+  }else{
+		res.status(404).send("No Data Found");
+
+  }
 	
   });
 });
