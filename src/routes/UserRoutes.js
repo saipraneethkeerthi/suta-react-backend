06ffdc69514dcd0305bd30418fdc3c5b2daf5256
Keerthi Sai Praneeth <kpraneeth@innominds.com>
2021-11-22 19:26:11 +0530

    added jwt token for login

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -1,29 +1,32 @@
 //Importing express and router
-const express = require('express');
-const user = require('../schema/userschema');
-const config = require('../config.json');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const user = require("../schema/userschema");
+const config = require("../config.json");
+const jwt = require("jsonwebtoken");
 const router = new express.Router();
 
-
-router.get('/userGetData', (req, res) => {
-	console.log("database post")
-	const storeddata = user.find({})
-	console.log(storeddata)
-	res.status(200).send('')
-})
-router.post('/signup', (req, res) => {
-	const body = req.body;
-	console.log(body)
-	user.insertMany(req.body).then((data) => res.status(200).send(data))
-})
-router.post('/login', (req, res) => {
-	const body = req.body
-	console.log(body)
-	user.find(body).then((data) => {
-		const token = jwt.sign({ sub: user.id }, config.secret, { expiresIn: '7d' });
-		console.log(token)
-		res.status(200).send(data)
-	})
-})
+router.get("/userGetData", (req, res) => {
+  console.log("database post");
+  const storeddata = user.find({});
+  console.log(storeddata);
+  res.status(200).send("");
+});
+router.post("/signup", (req, res) => {
+  const body = req.body;
+  console.log(body);
+  user.insertMany(req.body).then((data) => res.status(200).send(data));
+});
+router.post("/login", (req, res) => {
+  const body = req.body;
+  console.log(body);
+  user.findOne(body).then((data) => {
+    const tokenGen = jwt.sign({ sub: data._id }, config.secret, {
+      expiresIn: "7d",
+    });
+    const newDate = { ...data._doc, token: tokenGen };
+    // data.token=tokenGen
+    // console.log(newDate)
+    res.status(200).send(newDate);
+  });
+});
 module.exports = router;
